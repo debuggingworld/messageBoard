26191d3d4a015ce90a7d10350d3054f2fe5ab9c9
Zhangtao153 <13289265979@163.com>
2019-04-02 01:08:55 +0800

    Servlet_old

--- src/com/zth/servlet/core/HelloServlet.java
+++ src/com/zth/servlet/core/HelloServlet.java
@@ -1,4 +1,28 @@
 package com.zth.servlet.core;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
 public class HelloServlet extends ServletBase {
+    @Override
+    public void index(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.rander(resp,"index 方法");
+    }
+
+    public void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.rander(resp,"add 方法");
+    }
+
+    public void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.rander(resp,"delete 方法");
+}
+
+    public void update(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.rander(resp,"update 方法");
+    }
+
 }
